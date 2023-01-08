fe6d0366d270c39df97bdba35abf15e3d79016cf
Jae <slee0190@student.monash.edu>
2023-01-08 23:43:28 +1100

    Dev Todolist (Create main post page and linked with other Activities)

--- app/src/main/java/com/example/todolistapp/HomeActivity.java
+++ app/src/main/java/com/example/todolistapp/HomeActivity.java
@@ -1,14 +1,60 @@
 package com.example.todolistapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private RecyclerView recyclerView;
+    private FloatingActionButton floatingActionButton;
+    //layout에서 썻던 함수들을 여기다가 옮겨서 연결시켜줌
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //초기화를먼저 시켜준다
+        toolbar = findViewById(R.id.homeToolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Todo List App");
+
+        recyclerView = findViewById(R.id.recyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        floatingActionButton = findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addTask();
+            }
+        });
+    }
+    //다른공간들을 눌러도 cancel되지않게 한다
+
+    private void addTask() {
+        AlertDialog.Builder myDiaglog = new AlertDialog.Builder(this);
+        LayoutInflater inflater = LayoutInflater.from(this);
+
+        View myView = inflater.inflate(R.layout.input_file, null);
+        myDiaglog.setView(myView);
+
+        AlertDialog dialog = myDiaglog.create();
+        dialog.setCancelable(false);
+        dialog.show();
     }
 }
